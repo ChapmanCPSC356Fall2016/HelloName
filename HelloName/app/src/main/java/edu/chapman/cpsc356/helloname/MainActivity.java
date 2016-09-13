5a73a628911b8491194f1392915f79d083bec936
ryanburns31892 <rburns@chapman.edu>
2016-09-12 19:23:15 -0700

    Making some listener changes

--- HelloName/app/src/main/java/edu/chapman/cpsc356/helloname/MainActivity.java
+++ HelloName/app/src/main/java/edu/chapman/cpsc356/helloname/MainActivity.java
@@ -2,7 +2,11 @@ package edu.chapman.cpsc356.helloname;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,6 +14,8 @@ public class MainActivity extends AppCompatActivity
 {
     TextView nameTextView;
     EditText nameEditText;
+
+    Button goButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,9 +25,43 @@ public class MainActivity extends AppCompatActivity
 
         this.nameTextView = (TextView) findViewById(R.id.tv_name);
         this.nameEditText = (EditText) findViewById(R.id.et_name);
+        this.nameEditText.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+                Log.i("TEST", "beforeTextChanged");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+                Log.i("TEST", "onTextChange");
+                updateDisplay();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                Log.i("TEST", "afterTextChanged");
+            }
+        });
+
+        this.goButton = (Button) findViewById(R.id.btn_go);
+        this.goButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Log.i("TEST", "Button was pressed");
+                updateDisplay();
+            }
+        });
+
+        Log.i("TEST", "onCreate Finished here");
     }
 
-    public void goTapped(View view)
+    public void updateDisplay()
     {
         String name = nameEditText.getText().toString();
 
@@ -29,6 +69,7 @@ public class MainActivity extends AppCompatActivity
 
         this.nameTextView.setText(display);
 
-        this.nameTextView.setVisibility(View.VISIBLE);
+        int vis = name.isEmpty() ? View.GONE : View.VISIBLE;
+        this.nameTextView.setVisibility(vis);
     }
 }
